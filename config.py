314fb697b2c512a2415c37bcbe7d26362dde8934
Likvik <erik@elfisk.dk>
2025-11-04 12:04:25 +0100

    schmovement and gropple

--- config.py
+++ config.py
@@ -8,20 +8,30 @@ class collisionbox():
         self.size = size
         self.position = Vector2(position_x, position_y)
         self.velocity = Vector2(0, 0)
-        self.direction = Vector2(0.00001, 0.00001)
+        self.direction = Vector2(0, 0)
 
 
 
 class players(collisionbox):
-    def __init__(self, area_type,  size, position_x, position_y, maxspeed, speed):
+    def __init__(self, area_type,  size, position_x, position_y, speed, max_range):
         super().__init__(area_type, size, position_x, position_y)
         self.boost_speed = speed
-        self.max_speed = maxspeed
+        self.is_grappling = False
+        self.max_grapple_range = max_range
 
 
-#keys pressed:
- 
+
+
+#keys pressed
 w_pressed = False
 s_pressed = False
 a_pressed = False
 d_pressed = False
+
+mouse_pos = 0
+mouse_x = 0
+mouse_y = 0
+m1_pressed = False
+m2_pressed = False
+
+#other variables
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import config
 from config import players
 
 
-
-
 pygame.init()
 
 screen = pygame.display.set_mode((1000 , 600), 0)
@@ -14,23 +12,52 @@ width, height = screen.get_size()
 
 
 
-player = players("rect", 20, 100, 100, 0.1, 0.01)
+player = players("rect", #areatype
+                 20, #size
+                 100, #start position_x
+                 100, #start position_y
+                 0.5, #boost_speed
+                 200 #max grapple range
+                 )
 
 #player_img = pygame.image.load("img/player.png").convert_alpha()
 #player_img = pygame.transform.scale(player_img, (200, 150))
 
+grapple_position = Vector2(200, 200)
+
+def boost():
+    if config.w_pressed:
+        player.velocity.y -= player.boost_speed
+
+    if config.a_pressed:
+        player.velocity.x -= player.boost_speed
+
+    if config.s_pressed:
+        player.velocity.y += player.boost_speed
+
+    if config.d_pressed:
+        player.velocity.x += player.boost_speed
+
+def looP():
+    if player.position.x < 0:
+        player.position.x = width - player.size
+    if player.position.x > width - player.size:
+        player.position.x = 0
+    if player.position.y < 0:
+        player.position.y = height - player.size
+    if player.position.y > height - player.size:
+        player.position.y = 0
+
+def grapple(x, y):
+    grapple_position = Vector2(x, y)
 
 
-#print(player.direction.angle_to(Vector2(1,1)))
 
-
-
+clock = pygame.time.Clock()
 running = True
 while running:   
     screen.fill((0, 0, 0))
 
-    
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -67,46 +94,36 @@ while running:
                 config.d_pressed = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()  
-            mouse_x, mouse_y = pygame.mouse.get_pos()
+            config.mouse_pos = pygame.mouse.get_pos()  
+            config.mouse_x, config.mouse_y = pygame.mouse.get_pos()
+            grapple()
     
 
-    sped = 0.005
-    if config.w_pressed:
-        player.direction.y -= sped
+    boost()
+    #looP()
 
-    if config.a_pressed:
-        player.direction.x -= sped
+    if player.position.distance_to(grapple_position) > player.max_grapple_range:
+        grapple_vector = player.position - grapple_position
 
-    if config.s_pressed:
-        player.direction.y += sped
+        player.direction = Vector2(-grapple_vector.y, grapple_vector.x).normalize() #sætter retningen til at være retvinklet med snoren
 
-    if config.d_pressed:
-        player.direction.x += sped
+        player.direction = player.direction * player.velocity.length()
 
+        #player.velocity = player.direction
+
+
+
+
+    if player.velocity.length() != 0:
+        player.direction = player.velocity.normalize()
     
-    if player.position.x < 0:
-        player.position.x = width - player.size
-    if player.position.x > width - player.size:
-        player.position.x = 0
-    if player.position.y < 0:
-        player.position.y = height - player.size
-    if player.position.y > height - player.size:
-        player.position.y = 0
-
-    player.direction = player.direction.normalize()
-
-    print(player.direction)
-
-    player.velocity = player.direction * 0.3
-    player.position.x += player.velocity.x
-    player.position.y += player.velocity.y
+    player.position += player.velocity
     pygame.draw.rect(screen, (0, 255, 0), (player.position.x, player.position.y, player.size, player.size))
-    #print(player.position.x)
-    #print(player.position.y)
+
+
     
-        
     pygame.display.flip()
+    clock.tick(60)
     
 
 pygame.quit()
